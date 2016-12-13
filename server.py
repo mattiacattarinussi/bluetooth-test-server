a473831655bfefe508f4e0860bde05ae3b320e2b
Mattia Cattarinussi <mcattarinussi@gmail.com>
2016-12-13 03:24:29 +0100

    Handle client disconnect

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ def handle_stdin(client_sock):
         print('> ', end='', flush=True)
 
 
-def listen_for_data(client_sock):
+def read_data(client_sock):
     data = client_sock.recv(1024)
     return data.decode('utf-8')
 
@@ -24,25 +24,39 @@ async def handle_console_output(client_sock, queue):
         print('\rReceived> : ', data, '\n> ', end='', flush=True)
 
 
-async def handle_client_data(client_sock, loop, queue):
+async def listen_for_data(client_sock, loop, queue):
     while True:
-        data = await asyncio.ensure_future(loop.run_in_executor(None, listen_for_data, client_sock), loop=loop)
-        asyncio.ensure_future(queue.put(data), loop=loop)
+        try:
+            bluetooth.btcommon.BluetoothError
+            data = await asyncio.ensure_future(loop.run_in_executor(None, read_data, client_sock), loop=loop)
+            asyncio.ensure_future(queue.put(data), loop=loop)
+        except bluetooth.btcommon.BluetoothError as e:
+            return False
+
+async def handle_client_data(client_sock, loop, queue):
+    output_cor = asyncio.ensure_future(handle_console_output(client_sock, queue), loop=loop)
+    result = await listen_for_data(client_sock, loop, queue)
+    if not result:
+        output_cor.cancel()
+    return result
 
 
 def main(server_sock):
     client_sock = None
+    loop = asyncio.get_event_loop()
+    queue = asyncio.Queue(loop=loop)
     try:
         while True:
             print('\n***   Waiting for connection...   ***')
             client_sock, address = server_sock.accept()
             print('\n***   Accepted connection from {}   ***\n'.format(address[0]))
             # Run main loop
-            loop = asyncio.get_event_loop()
-            queue = asyncio.Queue(loop=loop)
-            asyncio.ensure_future(handle_client_data(client_sock, loop, queue), loop=loop)
             loop.add_reader(sys.stdin, handle_stdin, client_sock)
-            loop.run_until_complete(handle_console_output(client_sock, queue))
+            listener = asyncio.ensure_future(handle_client_data(client_sock, loop, queue), loop=loop)
+            loop.run_until_complete(listener)
+            if not listener.result():
+                loop.remove_reader(sys.stdin)
+                print('\n*** Connection closed by the client!! ***')
     except KeyboardInterrupt:
         print('\n***   Server stopped by user   ***')
 
